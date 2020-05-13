efd47efb4e0cfffc994fb9b1faa007e3c1802ba1
Natsu Dragoneel <pranay.aark@gmail.com>
2020-05-13 18:53:32 +0530

    first commit

---
+++ app/src/main/java/com/aarktechhub/loginsystem/ForgotPasswordActivity.java
@@ -0,0 +1,44 @@
+package com.aarktechhub.loginsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+    }
+
+    //hide keyboard on click outside
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            View v = getCurrentFocus();
+            if (v instanceof EditText) {
+                Rect outRect = new Rect();
+                v.getGlobalVisibleRect(outRect);
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
+                    v.clearFocus();
+                    InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event);
+    }
+
+    public void loginBack(View view) {
+        startActivity(new Intent(ForgotPasswordActivity.this,MainActivity.class));
+        overridePendingTransition(R.anim.slide_in_up,R.anim.slide_out_down);
+    }
+}
